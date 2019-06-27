49b6ee871bda5f3cafc300875800787cfd822c49
koocieyu <koocieyu@gmail.com>
2019-06-27 23:41:06 +0300

    Adaugat problema 2Pare

---
+++ probleme-pbinfo/c++/2Pare.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0) cout << n + 2 << " " << n + 4;
+    else cout << n + 1 << " " << n + 3;
+    return 0;
+}
