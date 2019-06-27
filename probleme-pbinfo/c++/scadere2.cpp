4eb7c94984df1c5a96ecd0c9834acd98f765a789
koocieyu <koocieyu@gmail.com>
2019-06-27 21:36:46 +0300

    Adaugat problema scadere2

---
+++ probleme-pbinfo/c++/scadere2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+	int descazut, scazator, rezultat;
+	std::cin >> descazut;
+	std::cin >> scazator;
+
+	rezultat = descazut - scazator;
+	std::cout << rezultat;
+	return 0;
+}
