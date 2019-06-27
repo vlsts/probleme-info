235bfc05f92d062736b02c53c5735fc67b37c461
koocieyu <koocieyu@gmail.com>
2019-06-27 23:41:27 +0300

    Adaugat problema max2

---
+++ probleme-pbinfo/c++/max2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    int numar1, numar2;
+    std::cin >> numar1 >> numar2;
+
+    if (numar1 > numar2) { std::cout << numar1; }
+    else { std::cout << numar2; }
+    return 0;
+}
