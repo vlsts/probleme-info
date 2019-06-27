516f6fd2c853d162362fe2ef80895ba2f0839849
koocieyu <koocieyu@gmail.com>
2019-06-27 23:41:49 +0300

    Adaugat problema ultimacifraapartiiintregi

---
+++ probleme-pbinfo/c++/ultimacifraapartiiintregi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    double a;
+    cin >> a;
+
+    int rotunjit = floor(a);
+
+    cout << rotunjit % 10;
+
+    return 0;
+}
