3fc75e015da56f75473fa0ad162d11d98ea46366
koocieyu <koocieyu@gmail.com>
2019-06-27 23:41:48 +0300

    Adaugat problema triunghi

---
+++ probleme-pbinfo/c++/triunghi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cin >> a >> b >> c;
+    
+    if (a <= 0 || b <= 0 || c <= 0) cout << "nu";
+    else if (a + b <= c || a + c <= b || b + c <= a) cout << "nu";
+    else cout << "da";
+    return 0;
+}
