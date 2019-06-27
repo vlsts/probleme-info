de5e561f82bfc8b737f54e6dbe835195582bc7e1
koocieyu <koocieyu@gmail.com>
2019-06-27 23:41:18 +0300

    Adaugat problema cub

---
+++ probleme-pbinfo/c++/cub.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    int S = 6 * a * a;
+    int V = a * a * a;
+
+    cout << S << " " << V;
+
+    return 0;
+}
