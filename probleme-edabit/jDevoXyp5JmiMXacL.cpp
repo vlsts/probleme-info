715bd15fe34c135487b138ede5a61db6096107d8
Koocie <me@koocie.space>
2020-09-05 19:41:41 +0300

    Adaugat Edabit: Make a Circle with OOP

---
+++ probleme-edabit/jDevoXyp5JmiMXacL.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: Make a Circle with OOP
+Author: Alex Golubov
+*/
+
+// Given
+const float PI_F = 3.14f;
+
+class Circle {
+    private:
+        double raza;
+    public:
+        Circle(double);
+        double getArea();
+        double getPerimeter();
+};
+
+Circle::Circle (double r) : raza(r) {};
+
+double Circle::getArea() {
+    return PI_F * raza * raza;
+}
+
+double Circle::getPerimeter() {
+    return 2 * PI_F * raza;
+}
